a7467d028ad84f32d99311597003d8bd8c328fe8
Soheil Banayan <31982723+soheilbanayan@users.noreply.github.com>
2018-12-17 23:25:25 +1300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const route = express.Router();//function to execute
 const productsControllers = require('../controllers/products');
